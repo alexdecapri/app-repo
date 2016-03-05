19eac69b50e1c98928a75377ef03b8212eb7dc46
alexdecapri <alexdecapri@gmail.com>
2016-03-05 13:55:36 -0700

    through problem 3 on 3/3 Day 1

--- components/quiz/quizCtrl.js
+++ components/quiz/quizCtrl.js
@@ -1,9 +1,49 @@
 var app = angular.module("quizApp");
 
-app.controller("quizCtrl", function($scope, $stateParams) {
-  $scope.test = "on the quiz page";
-  console.log($stateParams);
-  $scope.quizName = $stateParams.quizName;
-  // $scope.questions = questions;
+app.controller("quizCtrl", function($scope, $stateParams, questions, quizService) { //refers to line 22 app.js resolve questions
+  // console.log($stateParams);
+  // $scope.quizName = $stateParams.quizName; to show us that it was there, deleted on 3/3 of Day 1
+  $scope.questions = questions;
+  $scope.answers = {};
+  $scope.currentQuestion = $scope.questions[0];
+  $scope.results = {};
+
+  $scope.saveAnswer = function(answer) {
+    $scope.answers[$scope.currentQuestion.id] = answer;
+    $scope.nextQuestion();
+    if ($scope.results.done) {
+      //we've already hit "check answrs so update the answer results"
+      $scope.checkMyAnswers();
+    }
+  };
+
+  $scope.setCurrentQuestion = function(question) {
+    $scope.currentQuestion = question;
+  }
+
+  $scope.handleEnter = function(ev, answer) {
+    /* black diamond to make this work*/
+  }
+
+  $scope.nextQuestion = function() {
+    var idx = $scope.questions.indexOf($scope.currentQuestion);
+    if ($scope.questions[idx + 1]) {
+      $scope.currentQuestion = $scope.questions[idx + 1];
+    }
+    else {
+      return;
+    }
+  }
+
+  $scope.checkMyAnswers = function() {
+    quizService.checkMyAnswers($scope.questions, $scope.answers).then(function (response) {
+      $scope.results = response;
+    });
+  }
+
+  $scope.reset = function() {
+    $scope.answers = {};
+    $scope.currentQuestion = $scope.questions[0];
+  }
 
 });
